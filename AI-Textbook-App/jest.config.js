19fa0adf0754fb9d0e293b0b89a732c3d317c0f6
Lucas Gomes <lucasgomes@Craigs-MacBook-Pro.local>
2026-01-23 15:19:51 -0800

    undo change to jest config

--- AI-Textbook-App/jest.config.js
+++ AI-Textbook-App/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'jest-expo',
+  preset: 'react-native',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native|expo|@expo|expo-.*))'
